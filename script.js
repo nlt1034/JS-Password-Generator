72923ab70c81256bc0647335f50895425a9a96e9
Nate Taylor <nlt1034@uncw.edu>
2021-02-23 02:37:30 -0500

    Added script.js

---
+++ script.js
@@ -0,0 +1,68 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Assign variables
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "0123456789";
+var special = "!@,#$%&*{}[]/\\+=";
+var chosenCharacters = "";
+
+// Generate password function
+function generatePassword(){
+  var result = "";
+
+  // How many characters
+  var length = prompt("How many characters in your random password? (between 8 and 128)");
+  if(isNaN(length)){
+    alert("You must input a number!");
+    return generatePassword()
+  }
+  if(length<8 || length> 128){
+    alert("Please choose numbers between 8 - 128!");
+    return generatePassword()
+  }
+  // Include Uppercase, lowerscase, numeric, special characters?
+  var hasUpper = confirm("Include upper case letters?");
+  var hasLower = confirm("Include lower case letters?");
+  var hasNumbers = confirm("Include numbers?");
+  var hasSpecial = confirm("Include special characters?");
+
+  if(!hasUpper&&!hasLower&&!hasNumbers&&!hasSpecial){
+    alert("You must at least choose 1 character type!");
+    return generatePassword()
+  }
+
+  if(hasUpper){
+    chosenCharacters += upper
+  }
+  if(hasLower){
+    chosenCharacters += lower
+  }
+
+  if(hasNumbers) {
+    chosenCharacters += numbers
+  }
+
+  if(hasSpecial) {
+    chosenCharacters += special
+  }
+
+for (var i = 0; i < length; i++) {
+  result += chosenCharacters.charAt(Math.floor(Math.random() * chosenCharacters.length));
+}
+return result;
+
+}
